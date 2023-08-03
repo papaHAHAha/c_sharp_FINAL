51097a3039069820bca40efbdbfa7fd3d4877425
kirill <golovinkiruxa@gmail.com>
2023-08-03 17:42:55 +0200

    task and new readme

--- Program.cs
+++ Program.cs
@@ -1,1 +1,46 @@
-﻿
+﻿using System;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    string[] inputArray = { "short", "cut", "cat", "dog", "long" };
+
+    string[] resultArray = FilterArray(inputArray);
+
+    Console.WriteLine("Результат:");
+    foreach (string str in resultArray)
+    {
+      Console.WriteLine(str);
+    }
+  }
+
+  static string[] FilterArray(string[] inputArray)
+  {
+    int resultSize = 0;
+
+    // Считаем количество строк, длина которых <= 3
+    foreach (string str in inputArray)
+    {
+      if (str.Length <= 3)
+      {
+        resultSize++;
+      }
+    }
+
+    string[] resultArray = new string[resultSize];
+    int index = 0;
+
+    // Заполняем новый массив элементами, длина которых <= 3
+    foreach (string str in inputArray)
+    {
+      if (str.Length <= 3)
+      {
+        resultArray[index] = str;
+        index++;
+      }
+    }
+
+    return resultArray;
+  }
+}
